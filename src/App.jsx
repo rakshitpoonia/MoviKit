01a41ba7bedc17867e2c1aa69574714e69f7e7d3
Rakshit Poonia <rakshitpoonia2004@gmail.com>
2025-08-05 16:49:08 +0530

    Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Favorites from "./pages/Favorites";
 import { MovieProvider } from "./contexts/MovieContext";
 import { ModalProvider } from "./contexts/ModalContext";
 import GlobalModal from "./components/GlobalModal";
+import { Analytics } from '@vercel/analytics/react';
+
 
 
 // Main App component with routing
@@ -16,6 +18,7 @@ const App = () => {
           <Route path="/favorites" element={<Favorites />} />
         </Routes>
         <GlobalModal />
+        <Analytics />
       </ModalProvider>
     </MovieProvider>
   );
